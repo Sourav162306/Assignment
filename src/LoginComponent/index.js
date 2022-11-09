cbf44d36fcd2e127e195393a1db418261dc3fb8a
Sourav Paul <111589971+Sourav162306@users.noreply.github.com>
2022-11-10 01:50:08 +0530

    Update index.js
    msg

--- src/LoginComponent/index.js
+++ src/LoginComponent/index.js
@@ -4,21 +4,65 @@ import { Link } from "react-router-dom";
 import "./index.css";
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { email, password } = this.state;
+    console.log(email, password);
+    fetch("http://localhost:3000/login-user", {
+      method: "POST",
+      crossDomain: true,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data, "userRegister");
+        if (data.status === "ok") {
+          alert("login successful");
+          window.localStorage.setItem("token", data.data);
+          window.location.href = "./userDetails";
+        }
+      });
+  }
+
+  onChangeEmail = (event) => {
+    this.setState({ email: event.target.value });
+  };
+
+  onChangePass = (event) => {
+    this.setState({ password: event.target.value });
+  };
+
   render() {
     return (
       <form className="login-container">
         <h1 className="signin-heading">Sign In</h1>
 
-        <div className="mb-3">
+        <div className="">
           <label>Email address</label>
           <br />
-          <input type="email" className="" placeholder="Enter email" />
+          <input onChange={this.onChangeEmail} type="email" className="" placeholder="Enter email" />
         </div>
 
         <div className="">
           <label>Password</label>
           <br />
-          <input type="password" className="" placeholder="Enter password" />
+          <input onChange={this.onChangePass} type="password" className="" placeholder="Enter password" />
         </div>
         <br />
         <div className="">
